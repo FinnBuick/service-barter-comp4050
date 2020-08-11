548d8e060827cfe7e307485519ed652809672d44
Kane <kyung-hyun.shin@students.mq.edu.au>
2020-08-11 12:01:07 +1000

    Little update for home ui

--- service-barter-react/src/components/home/home.tsx
+++ service-barter-react/src/components/home/home.tsx
@@ -1,5 +1,7 @@
-import { Typography } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 import * as React from "react";
 
 import styles from "./home.scss";
@@ -18,11 +20,20 @@ export const Home = React.memo(() => (
       </div>
     </div>
     <div className={styles.contentBody}>
-      <Typography>
+      <Typography variant="h5">
         List of recent favours (In your area)
       </Typography>
-      <Typography>
-        List of recent favours history
+      <Grid container spacing={2} className={styles.grid}>
+        <Grid container justify="center">
+          {[0, 1, 2].map((value) => (
+            <Grid key={value} item>
+              <Paper elevation={3} className={styles.paper} />
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+      <Typography variant="h5">
+        List of recent favour history
       </Typography>
     </div>
   </div>
