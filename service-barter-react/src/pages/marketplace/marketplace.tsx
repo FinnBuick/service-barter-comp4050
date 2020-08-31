5b5efe081ebc98f8f2ba8d48f1ed0f73be692525
Alex Grbic <alexander.grbic@students.mq.edu.au>
2020-09-01 08:02:14 +1000

    Added favour creation to marketplace

--- service-barter-react/src/pages/marketplace/marketplace.tsx
+++ service-barter-react/src/pages/marketplace/marketplace.tsx
@@ -1,3 +1,4 @@
+import { CardActionArea, CircularProgress } from "@material-ui/core";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
@@ -7,17 +8,38 @@ import CardHeader from "@material-ui/core/CardHeader";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
+import * as firebase from "firebase";
 import * as React from "react";
 
+import { UserContext } from "../../components/user/user_provider";
 import styles from "./marketplace.scss";
 
-export class Marketplace extends React.Component {
-  favours = [
-    { title: "title", description: "" },
-    { title: "title", description: "" },
-    { title: "title", description: "" },
-  ];
-  FavourCard = (props) => {
+type Favours = {
+  id: string;
+  title: string;
+};
+export class Marketplace extends React.Component<
+  unknown,
+  {
+    id: undefined;
+    favourList: Favours[];
+  }
+> {
+  static contextType = UserContext;
+  private database?: firebase.database.Database;
+  private userContext: any;
+  private favourListener?: any;
+
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      id: undefined,
+      favourList: undefined,
+    };
+    this.userContext = context;
+  }
+
+  private FavourCard = React.memo(({ favour }: { favour: Favours }) => {
     return (
       <Paper>
         <Card>
@@ -27,7 +49,7 @@ export class Marketplace extends React.Component {
                 A
               </Avatar>
             }
-            title={props.title}
+            title={favour.title}
             subheader="September 14, 2016"
           />
           <CardContent>
@@ -38,9 +60,7 @@ export class Marketplace extends React.Component {
             >
               Location
             </Typography>
-            <Typography variant="body2" component="p">
-              {props.description}
-            </Typography>
+            <Typography variant="body2" component="p"></Typography>
           </CardContent>
           <CardActions>
             <Button size="small">Learn More</Button>
@@ -48,15 +68,33 @@ export class Marketplace extends React.Component {
         </Card>
       </Paper>
     );
-  };
-  MemoFavourCard = React.memo(this.FavourCard);
+  });
 
+  componentDidMount() {
+    this.database = firebase.database();
+    if (this.userContext.loggedIn) {
+      this.getFavours();
+    }
+  }
+
+  componentDidUpdate() {
+    if (this.userContext != this.context) {
+      this.userContext = this.context;
+      if (this.userContext.loggedIn) {
+        this.getFavours();
+      }
+    }
+  }
   render() {
     return (
       <div className={styles.content}>
         <div className={styles.buttonsWrapper}>
           <div>
-            <Button className={styles.buttons} variant="contained">
+            <Button
+              className={styles.buttons}
+              variant="contained"
+              onClick={this.createFavour}
+            >
               + Add New Favour
             </Button>
           </div>
@@ -77,18 +115,67 @@ export class Marketplace extends React.Component {
             </Button>
           </div>
         </div>
-
         <div className={styles.cardsWrapper}>
           <Typography>Cards</Typography>
           <Grid container spacing={2}>
-            {this.favours.map((favour) => (
-              <Grid key={favour.title} item md={3}>
-                <this.FavourCard title={favour.title} />
-              </Grid>
-            ))}
+            <Grid item md={3}>
+              {this.state.favourList == null ? (
+                <>
+                  <CircularProgress />
+                  <br />
+                </>
+              ) : this.state.favourList.length === 0 ? (
+                <Typography>
+                  There are no favours, start by creating one!
+                </Typography>
+              ) : (
+                <>
+                  {this.state.favourList.map((favour) => (
+                    <this.FavourCard key={favour.id} favour={favour} />
+                  ))}
+                </>
+              )}
+            </Grid>
           </Grid>
         </div>
       </div>
     );
   }
+
+  getFavours() {
+    const user = this.userContext.user;
+    this.database.ref(`/users/${user.uid}/favours`).on("value", (snapshot) => {
+      if (snapshot.exists()) {
+        this.setState((state) => ({
+          ...state,
+          id: undefined,
+          favourList: Object.values(snapshot.val()),
+        }));
+      } else {
+        this.setState((state) => ({
+          ...state,
+          id: undefined,
+          favourList: [],
+        }));
+      }
+    });
+  }
+
+  createFavour = () => {
+    const favourRef = this.database.ref().child("/favours").push();
+    favourRef.set({
+      id: favourRef.key,
+      favourList: [],
+    });
+
+    const user = this.userContext.user;
+    console.log(user);
+    const favourName = `${"word"}${"word"}`;
+    const favour = {
+      id: favourRef.key,
+      name: favourName,
+    };
+
+    this.database.ref(`/users/${user.uid}/favours`).push().set(favour);
+  };
 }
