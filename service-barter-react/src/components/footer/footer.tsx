33b37b079a9fc413bd03c27831a94718b151e951
James Ridey <james.r@canva.com>
2020-09-01 12:41:14 +1000

    Add an snackbar alert box for a password reset

--- service-barter-react/src/components/footer/footer.tsx
+++ service-barter-react/src/components/footer/footer.tsx
@@ -2,6 +2,4 @@ import * as React from "react";
 
 import styles from "./footer.scss";
 
-export const Footer = React.memo(() => (
-  <div className={styles.footer}>footer</div>
-));
+export const Footer = React.memo(() => <div className={styles.footer} />);
--- service-barter-react/src/pages/profile/profile.tsx
+++ service-barter-react/src/pages/profile/profile.tsx
@@ -1,7 +1,9 @@
+import { Snackbar } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import RoomIcon from "@material-ui/icons/Room";
+import MuiAlert from "@material-ui/lab/Alert";
 import * as firebase from "firebase";
 import * as React from "react";
 import { Link } from "react-router-dom";
@@ -11,75 +13,99 @@ import styles from "./profile.scss";
 
 export const Profile = React.memo(() => {
   const userContext = React.useContext(UserContext);
+  const [snackbarOpen, setSnackbarOpen] = React.useState(false);
 
   const resetPassword = () =>
     firebase
       .auth()
       .sendPasswordResetEmail(firebase.auth().currentUser.email)
       .then(() => {
-        location.reload();
+        setSnackbarOpen(true);
       });
+
+  const handleSnackbarClose = () => setSnackbarOpen(false);
 
   return (
     <div className={styles.content}>
       {userContext.user && (
-        <div className={styles.profile}>
-          <Typography
-            variant="h5"
-            style={{ display: "inline-block", marginLeft: "30px" }}
+        <>
+          <Snackbar
+            open={snackbarOpen}
+            autoHideDuration={2000}
+            onClose={handleSnackbarClose}
           >
-            {userContext.user.displayName}
-          </Typography>
-          <Typography
-            variant="subtitle2"
-            style={{ display: "inline-block", marginLeft: "20px" }}
-          >
-            {userContext.user.address}
-          </Typography>
-          <RoomIcon />
-          <div className={styles.settingButtons}>
-            <Button
-              variant="contained"
-              color="primary"
-              component={Link}
-              to="/editProfile"
-              style={{ marginRight: "10px" }}
+            <MuiAlert
+              elevation={6}
+              variant="filled"
+              onClose={handleSnackbarClose}
+              severity="success"
             >
-              Edit profile
-            </Button>
-            <Button variant="contained" color="primary" onClick={resetPassword}>
-              Reset Password
-            </Button>
-          </div>
-          <div className={styles.profileBody}>
-            <div>
-              <div style={{ display: "inline-block" }}>
-                <Typography variant="h6">Email address</Typography>
-                <Typography variant="h6">Favour points</Typography>
-              </div>
-              <div style={{ display: "inline-block", marginLeft: "30px" }}>
-                <Typography variant="h6" style={{ color: "#0066ff" }}>
-                  {userContext.user.email}
-                </Typography>
-                <Typography variant="h6" style={{ color: "#0066ff" }}>
-                  {userContext.user.favourPoint}
-                </Typography>
-              </div>
+              A password reset email has been sent to your email,{" "}
+              {userContext.user.email}
+            </MuiAlert>
+          </Snackbar>
+          <div className={styles.profile}>
+            <Typography
+              variant="h5"
+              style={{ display: "inline-block", marginLeft: "30px" }}
+            >
+              {userContext.user.displayName}
+            </Typography>
+            <Typography
+              variant="subtitle2"
+              style={{ display: "inline-block", marginLeft: "20px" }}
+            >
+              {userContext.user.address}
+            </Typography>
+            <RoomIcon />
+            <div className={styles.settingButtons}>
+              <Button
+                variant="contained"
+                color="primary"
+                component={Link}
+                to="/editProfile"
+                style={{ marginRight: "10px" }}
+              >
+                Edit profile
+              </Button>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={resetPassword}
+              >
+                Reset Password
+              </Button>
             </div>
-            <Paper elevation={6} style={{ marginTop: "50px" }}>
-              <div className={styles.infoBox}>
-                <Typography variant="h5">History</Typography>
-                <Typography variant="subtitle1">Contents</Typography>
+            <div className={styles.profileBody}>
+              <div>
+                <div style={{ display: "inline-block" }}>
+                  <Typography variant="h6">Email address</Typography>
+                  <Typography variant="h6">Favour points</Typography>
+                </div>
+                <div style={{ display: "inline-block", marginLeft: "30px" }}>
+                  <Typography variant="h6" style={{ color: "#0066ff" }}>
+                    {userContext.user.email}
+                  </Typography>
+                  <Typography variant="h6" style={{ color: "#0066ff" }}>
+                    {userContext.user.favourPoint}
+                  </Typography>
+                </div>
               </div>
-            </Paper>
-            <Paper elevation={6} style={{ marginTop: "30px" }}>
-              <div className={styles.infoBox}>
-                <Typography variant="h5">Reviews</Typography>
-                <Typography variant="subtitle1">Contents</Typography>
-              </div>
-            </Paper>
+              <Paper elevation={6} style={{ marginTop: "50px" }}>
+                <div className={styles.infoBox}>
+                  <Typography variant="h5">History</Typography>
+                  <Typography variant="subtitle1">Contents</Typography>
+                </div>
+              </Paper>
+              <Paper elevation={6} style={{ marginTop: "30px" }}>
+                <div className={styles.infoBox}>
+                  <Typography variant="h5">Reviews</Typography>
+                  <Typography variant="subtitle1">Contents</Typography>
+                </div>
+              </Paper>
+            </div>
           </div>
-        </div>
+        </>
       )}
     </div>
   );
