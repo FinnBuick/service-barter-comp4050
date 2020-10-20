146ed6a694c71b94f77ace39734b27fc51268769
Kane <kyung-hyun.shin@students.mq.edu.au>
2020-10-20 11:32:56 +1100

    Update report ui

--- service-barter-react/src/pages/report/report.tsx
+++ service-barter-react/src/pages/report/report.tsx
@@ -1,4 +1,6 @@
 import Button from "@material-ui/core/Button";
+import FormControl from "@material-ui/core/FormControl";
+import NativeSelect from "@material-ui/core/NativeSelect";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
 import * as firebase from "firebase";
@@ -13,9 +15,10 @@ export const Report = React.memo(() => {
   const [newUserData, setNewUserData] = React.useState({
     name: "",
     email: "",
-    category: "",
+    category: "Fake user",
     description: "",
   });
+  const [categoryOthers, setCategoryOthers] = React.useState(false);
 
   React.useEffect(() => {
     if (!userContext.user) {
@@ -27,21 +30,29 @@ export const Report = React.memo(() => {
     setNewUserData({ ...newUserData, [e.target.name]: e.target.value });
   };
 
+  const onCategoryChange = (e) => {
+    const reportCategory = e.target.value;
+    if (reportCategory === "Others") {
+      setNewUserData({ ...newUserData, category: "" });
+      setCategoryOthers(true);
+    } else {
+      setNewUserData({ ...newUserData, category: reportCategory });
+      setCategoryOthers(false);
+    }
+  };
+
   const onSubmit = () => {
-    firebase.firestore().collection("users").doc(userContext.user.uid).update({
-      displayName: newUserData.name,
-      email: newUserData.email,
-    });
+    // firebase.firestore().collection("reports").doc(userContext.user.uid).update({
+    //   displayName: newUserData.name,
+    //   email: newUserData.email,
+    // });
   };
 
   return (
     <div className={styles.content}>
       {userContext.user && (
         <div className={styles.report}>
-          <Typography
-            variant="h4"
-            style={{ marginLeft: "30px", textAlign: "center" }}
-          >
+          <Typography variant="h4" style={{ textAlign: "center" }}>
             Report an issue
           </Typography>
           <div className={styles.reportInfo}>
@@ -65,23 +76,31 @@ export const Report = React.memo(() => {
                 onChange={(e) => onChange(e)}
               />
             </div>
-            <div className={styles.inputField}>
+            <div className={styles.categoryField}>
               <Typography className={styles.text} variant="h6">
                 Category
               </Typography>
-              <TextField
-                className={styles.textField}
-                name="email"
-                // defaultValue={userContext.user.email}
-                onChange={(e) => onChange(e)}
-              />
+              <FormControl>
+                <NativeSelect onChange={onCategoryChange}>
+                  <option>Fake user</option>
+                  <option>Incorrect user information</option>
+                  <option>Account stolen</option>
+                  <option>Bug found</option>
+                  <option>Others</option>
+                </NativeSelect>
+              </FormControl>
+              {categoryOthers && (
+                <TextField
+                  className={styles.othersField}
+                  placeholder="Please specify"
+                  name="category"
+                  onChange={(e) => onChange(e)}
+                />
+              )}
             </div>
           </div>
           <div className={styles.reportContent}>
-            <Typography
-              variant="h6"
-              style={{ display: "inline-block", marginLeft: "30px" }}
-            >
+            <Typography variant="h6" style={{ marginLeft: "30px" }}>
               Description
             </Typography>
             <TextField
@@ -90,8 +109,27 @@ export const Report = React.memo(() => {
               rows={9}
               variant="outlined"
               onChange={onChange}
-              style={{ marginTop: "19px", marginLeft: "30px", width: "800px" }}
+              style={{ marginTop: "10px", marginLeft: "30px", width: "800px" }}
             />
+          </div>
+          <div style={{ marginTop: "9px", textAlign: "center" }}>
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={onSubmit}
+              component={Link}
+              to="/home"
+            >
+              Report
+            </Button>
+            <Button
+              variant="contained"
+              style={{ marginLeft: "4%" }}
+              component={Link}
+              to="/home"
+            >
+              Cancel
+            </Button>
           </div>
         </div>
       )}
