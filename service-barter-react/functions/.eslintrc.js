78c79f7aa293968fbc12a57b9085b9be3d90bb56
Finn Buick <finnbuick@gmail.com>
2020-10-20 13:24:01 +1100

    Fixed cloud functions imports and added James' code"

--- service-barter-react/functions/.eslintrc.js
+++ service-barter-react/functions/.eslintrc.js
@@ -13,10 +13,6 @@ module.exports = {
     project: "tsconfig.json",
     sourceType: "module",
   },
-  plugins: [
-    "@typescript-eslint",
-    "import",
-  ],
   rules: {
     "@typescript-eslint/adjacent-overload-signatures": "error",
     "@typescript-eslint/no-empty-function": "error",
--- service-barter-react/functions/src/index.ts
+++ service-barter-react/functions/src/index.ts
@@ -1,4 +1,6 @@
-import * as functions from 'firebase-functions';
+import * as admin from "firebase-admin";
+import * as functions from "firebase-functions";
+import { DataSnapshot } from "firebase-functions/lib/providers/database";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -8,9 +10,38 @@ import * as functions from 'firebase-functions';
 //     response.send("Hello from Firebase!");
 // });
 
-export const createNotificationOnFavourRequest = functions.firestore.document('requests/{favourId}')
-    .onUpdate((change, context) => {
-        const newValue = change.after.data();
+export const createNotificationOnFavourRequest = functions.firestore
+  .document("requests/{favourId}/requests/{userId}")
+  .onUpdate(
+    (change: functions.Change<any>, context: functions.EventContext) => {
+      const newValue = change.after.data();
 
-        functions.logger.log(newValue);
-    });
+      functions.logger.info("Favour request!", { structuredData: newValue });
+      console.log(context.params.favourId);
+    },
+  );
+
+export const updateMessageCount = functions.database
+  .ref("/rooms/{roomId}/messages")
+  .onCreate((snapshot: DataSnapshot, context: functions.EventContext) => {
+    return snapshot.ref.parent
+      ?.child("users")
+      .once("value")
+      .then((users) =>
+        users.val().map((user: string) => {
+          functions.logger.log(`Updating message count for ${user}`);
+
+          const roomRef = admin
+            .database()
+            .ref(`users/${user}/rooms/${context.params.roomId}`);
+          return roomRef.once("value").then((snap) => {
+            const value = snap.val();
+            functions.logger.log(`Value: ${value}`);
+            return roomRef.set({
+              ...value,
+              newMessages: value.newMessages + 1,
+            });
+          });
+        }),
+      );
+  });
