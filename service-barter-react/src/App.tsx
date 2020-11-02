94021ee0903638bd72cca613e5a5079c6a7e6184
Finn Buick <finnbuick@gmail.com>
2020-11-02 21:49:18 +1100

    redirect issue

--- service-barter-react/src/App.tsx
+++ service-barter-react/src/App.tsx
@@ -1,11 +1,10 @@
 import "firebase/database";
 import "./global.scss";
 
-import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
-import * as firebase from "firebase/app";
+import firebase from "firebase";
 import * as React from "react";
 import { hot } from "react-hot-loader";
-import { Route, Switch } from "react-router-dom";
+import { Route, Router, Switch } from "react-router-dom";
 
 import styles from "./App.scss";
 import { Footer } from "./components/footer/footer";
@@ -45,24 +44,6 @@ class App extends React.Component<
     this.setState({ sidebarOpen: !this.state.sidebarOpen });
   };
 
-  theme = createMuiTheme({
-    typography: {
-      fontFamily: `Roboto, sans-serif`,
-      fontSize: 14,
-      fontWeightLight: 300,
-      fontWeightRegular: 400,
-      fontWeightMedium: 500,
-    },
-    palette: {
-      primary: {
-        main: "#FF9C33",
-      },
-      secondary: {
-        main: "#4e636a",
-      },
-    },
-  });
-
   constructor(props) {
     super(props);
     this.state = { sidebarOpen: false };
@@ -72,65 +53,63 @@ class App extends React.Component<
 
   render() {
     return (
-      <ThemeProvider theme={this.theme}>
-        <UserProvider>
-          <div className={styles.app}>
-            <Sidebar
-              open={this.state.sidebarOpen}
-              setOpen={this.setSidebarOpen}
-            />
-            <div className={styles.header}>
-              <Header toggleSidebar={this.toggleSidebar} />
-            </div>
-            <div className={styles.content}>
-              <Switch>
-                <Route exact path="/">
-                  <Home />
-                </Route>
-
-                <Route exact path="/home">
-                  <Home />
-                </Route>
-
-                <Route path="/messaging">
-                  <Messaging />
-                </Route>
-
-                <Route path="/profile">
-                  <Profile />
-                </Route>
-
-                <Route path="/favours">
-                  <Favours />
-                </Route>
-
-                <Route path="/report">
-                  <Report />
-                </Route>
-
-                <Route path="/editProfile">
-                  <EditProfile />
-                </Route>
-
-                <Route path="/signin">
-                  <Signin />
-                </Route>
-
-                <Route path="/marketplace">
-                  <Marketplace />
-                </Route>
-
-                <Route path="*">
-                  <NoMatch />
-                </Route>
-              </Switch>
-            </div>
-            <div className={styles.footer}>
-              <Footer />
-            </div>
+      <UserProvider>
+        <div className={styles.app}>
+          <Sidebar
+            open={this.state.sidebarOpen}
+            setOpen={this.setSidebarOpen}
+          />
+          <div className={styles.header}>
+            <Header toggleSidebar={this.toggleSidebar} />
           </div>
-        </UserProvider>
-      </ThemeProvider>
+          <div className={styles.content}>
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+
+              <Route exact path="/home">
+                <Home />
+              </Route>
+
+              <Route path="/messaging">
+                <Messaging />
+              </Route>
+
+              <Route path="/profile">
+                <Profile />
+              </Route>
+
+              <Route path="/favours">
+                <Favours />
+              </Route>
+
+              <Route path="/report">
+                <Report />
+              </Route>
+
+              <Route path="/editProfile">
+                <EditProfile />
+              </Route>
+
+              <Route path="/signin">
+                <Signin />
+              </Route>
+
+              <Route path="/marketplace">
+                <Marketplace />
+              </Route>
+
+              <Route path="*">
+                <NoMatch />
+              </Route>
+            </Switch>
+          </div>
+          <div className={styles.footer}>
+            <Footer />
+          </div>
+        </div>
+      </UserProvider>
     );
   }
 }
